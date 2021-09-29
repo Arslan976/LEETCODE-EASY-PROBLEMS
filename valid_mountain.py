57ebd2a7a843b3a3b18694c544014e80f326c553
Arslan Faisal <programdev97@gmail.com>
2021-09-29 19:17:07 -0400

    made code changes

--- valid_mountain.py
+++ valid_mountain.py
@@ -1,42 +1,20 @@
-def validMountainArray(arr):
+class Solution(object):
+    def validMountainArray(self, arr):
         """
         :type arr: List[int]
         :rtype: bool
         """
-        strict_increase = True
-        index = 0
-        point = None
-        highest_peak = 0
-        
-        if len(arr) <= 2:
-            strict_increase = False
-        
-        while strict_increase != False and len(arr) > 2 and index < len(arr) :
-            print("in the loop")
-            if point == None:
-                point = arr[index]
-            elif point < arr[index]:
-                if index <= round(len(arr) / 2) - 1:     
-                    point = arr[index]
-                    highest_peak = index
-                else:
-                    strict_increase = False
-                    break
-            elif point == arr[index]:
-                strict_increase = False
-                break
-            elif point > arr[index]:
-                print(index)
-                if index > round(len(arr) / 2) - 1:    
-                    
-                     point = arr[index]
-                else:
-                    strict_increase = False
-                    break
+        if len(arr) < 3:
+            return False
+        index = 1    
+        while(index < len(arr) and arr[index] > arr[index - 1]):
             index += 1
-            
-        if highest_peak == len(arr) - 1:
-            strict_increase = False
-        print(strict_increase)
-        return strict_increase
-validMountainArray([1,3,2])
+        if index == 1 or index == len(arr):
+            return False
+        
+        while(index < len(arr) and arr[index] < arr[index - 1]):
+            index += 1
+        return index == len(arr)
+
+obj = Solution()
+print(obj.validMountainArray([0,3,2,1]))
